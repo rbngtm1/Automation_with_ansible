b212c30f51e43300b50672b379f9db707cfd7f07
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-03-11 23:00:33 -0500

    Update configure_ansible.sh

--- configure_ansible.sh
+++ configure_ansible.sh
@@ -1,17 +1,20 @@
-#!/bin/bash
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y epel-release
-sudo yum-config-manager --enable epel -y
-sudo yum repolist -y
+#cloud-boothook
+#!/bin/bash -xe
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console)2>&1
 sudo yum update -y
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+suod yum install ansible -y
 sudo yum install -y git python python-devel python-pip openssl ansible
-ansible --version
-sudo bash -c "cat /etc/ansible/ansible.cfg | sed 's/basic default values.../basic default values...\ninventory      = \/etc\/ansible\/hosts/g' | sed 's/basic default values...\nsudo_user     = root/g' > /etc/ansible/ansible.cfg.replacement"
-sudo cp /etc/ansible/ansible.cfg.replacement /etc/ansible/ansible.cfg
-sudo mv /etc/ansible/hosts /etc/hosts.original
-sudo bash -c 'echo "[local]" > /etc/ansible/hosts' && sudo bash -c 'echo "localhost ansible_connection=local" >> /etc/ansible/hosts'
-sudo useradd ansible
-#echo 'ansible' | sudo -S passwd ansible # Note the password of ansible user here is ansible
-sudo bash -c "cat /etc/sudoers | sed 's/.*group to run networking.*/ansible    ALL=(ALL)    NOPASSWD: ALL\n\n&/' | sed 's/.*group to run networking.*/ec2-user    ALL=(ALL)    NOPASSWD: ALL\n\n&/' > /etc/sudoers.placeholder"
-sudo cp /etc/sudoers.placeholder /etc/sudoers
 sudo pip install boto
+
+
+# above command will work in user data but below will work to set ansible all set in localhost as well, but not work as part of user data
+
+# sudo bash -c "cat /etc/ansible/ansible.cfg | sed 's/basic default values.../basic default values...\ninventory      = \/etc\/ansible\/hosts/g' | sed 's/basic default values...\nsudo_user     = root/g' > /etc/ansible/ansible.cfg.replacement"
+# sudo cp /etc/ansible/ansible.cfg.replacement /etc/ansible/ansible.cfg
+# sudo mv /etc/ansible/hosts /etc/hosts.original
+# sudo bash -c 'echo "[local]" > /etc/ansible/hosts' && sudo bash -c 'echo "localhost ansible_connection=local" >> /etc/ansible/hosts'
+# sudo useradd ansible
+# #echo 'ansible' | sudo -S passwd ansible # Note the password of ansible user here is ansible
+# sudo bash -c "cat /etc/sudoers | sed 's/.*group to run networking.*/ansible    ALL=(ALL)    NOPASSWD: ALL\n\n&/' | sed 's/.*group to run networking.*/ec2-user    ALL=(ALL)    NOPASSWD: ALL\n\n&/' > /etc/sudoers.placeholder"
+# sudo cp /etc/sudoers.placeholder /etc/sudoers
