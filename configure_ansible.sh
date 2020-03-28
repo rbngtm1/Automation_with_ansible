17a3360178ab99233154bc341c8b413de745ec91
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-03-28 15:00:23 -0500

    Update configure_ansible.sh

--- configure_ansible.sh
+++ configure_ansible.sh
@@ -2,8 +2,8 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console)2>&1
 sudo yum update -y
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-suod yum install ansible -y
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install ansible -y
 sudo yum install -y git python python-devel python-pip openssl ansible
 sudo pip install boto
 
